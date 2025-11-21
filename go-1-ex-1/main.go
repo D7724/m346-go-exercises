5e751a84347a9d611df4829f1cf5c9f28b4b12af
D7724 <Ryan_Truttmann@sluz.ch>
2025-11-21 14:40:40 +0100

    Aufgabe go-1-ex-1 gelöst

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Ryan"
+	var lastName string = "Trut"
+	var dayOfBirth int = 26
+	var monthOfBirth int = 12
+	var yearOfBirth int = 2008
+	var numberOfSiblings int = 4
+	var heightInMeters float32 = 3.0
+	var zodiacSign rune = '\u264B'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
